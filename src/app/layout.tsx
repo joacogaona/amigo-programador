c40419339d1e7413553e497cf453cd0bc7c7e7e2
joaco <jgaona@keiron.cl>
2023-10-18 02:14:36 -0300

    feat: tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+
+        <Script strategy="lazyOnload" id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+            });
+        `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
